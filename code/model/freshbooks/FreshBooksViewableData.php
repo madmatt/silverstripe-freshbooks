e804cf456b23eb75ef66f29d5222502117a7e720
madmatt <signups@madman.net.nz>
2014-07-14 13:05:45 +0000

    Update model classes

--- code/model/freshbooks/FreshBooksViewableData.php
+++ code/model/freshbooks/FreshBooksViewableData.php
@@ -4,6 +4,9 @@
  *
  * This class (and it's sub-classes) wrap around {@link ViewableData}, without saving any data to the database. This is
  * done to ensure we don't duplicate information in FreshBooks within the local SilverStripe data model.
+ *
+ * The {@link ViewableData::$casting} array should be declared to cast all values to equivalent field types, for use
+ * within SilverStripe templates.
  */
 class FreshBooksViewableData extends ViewableData {
 
