76fc7ab335b4fbdb891ee534beaeb6e1be012b52
madmatt <signups@madman.net.nz>
2014-07-16 01:59:17 +1200

    Fix scrutinizer error

--- code/gateways/FreshBooksSystemGateway.php
+++ code/gateways/FreshBooksSystemGateway.php
@@ -7,7 +7,7 @@ class FreshBooksSystemGateway extends FreshBooksBaseGateway {
 	/**
 	 * Normally, we would call this the name of the method, but `current` is a reserved keyword in PHP, so we can't.
 	 *
-	 * @return \GuzzleHttp\Message\ResponseInterface|null
+	 * @return SimpleXMLElement|null
 	 */
 	public function getSystem() {
 		return $this->call('current');
